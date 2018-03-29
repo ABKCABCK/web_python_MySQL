6d0209aad9ead9ff9492d9a52ca71619e105eb91
kobechao <chaokobe@gmail.com>
2018-03-29 11:37:35 +0800

    0329 fix csrf

--- Site/info/tests.py
+++ Site/info/tests.py
@@ -4,3 +4,6 @@ from __future__ import unicode_literals
 from django.test import TestCase
 
 # Create your tests here.
+from datetime import datetime
+print 'test'
+print datetime.now().strftime('%Y-%m-%d %H:%M:%S')
